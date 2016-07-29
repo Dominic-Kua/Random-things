0585db035798f424babaaf9c2efaafe32e65359c
Dominic Kua <dominic.kua@bromium.com>
2016-07-29 15:37:39 +0100

    initial commit, some comments, a skeleton of an outline. More to come

---
+++ link_scanner.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+'''
+This will hopefully evolve into a nice little link scanner
+which will pick up things like 403, 400 and hopeful 200s that show
+an error message when they should be returning a 404.
+'''
+#I think these two will be the main libraries I need,
+#might add collections if I start to store the URLs in something funky like
+#a named tuple.
+import requests
+import scrapy
+
+def url_fetcher(url):
+    '''
+    This will take the input URL and scan through the sub-domains
+    returning a list of them.
+    '''
+    pass
