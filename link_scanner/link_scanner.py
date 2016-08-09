59a45a6f8cefceea331d46677a4c58db957bcfff
Dominic Kua <dominic@kua.org.uk>
2016-08-09 18:17:30 +0100

    looks like the last nuke didn't take

--- link_scanner/link_scanner.py
+++ link_scanner/link_scanner.py
@@ -8,20 +8,3 @@ an error message when they should be returning a 404.
 #might add collections if I start to store the URLs in something funky like
 #a deque
 #if I thread it, and I might, it's going to end up needing asyncio I suspect.
-
-import requests
-
-
-def url_checked(url, url_list):
-	'''
-	Very simply, this function really only checks to see if
-	a URL has already been requested. If it hasn't it
-	adds it to the list and if it has then it doesn't
-	Then it returns true or false depending on if it needs
-	requesting or not.
-	'''
-	if url in url_list:
-		return url_list , False
-	else:
-		url_list.append(url)
-		return url_list , True
